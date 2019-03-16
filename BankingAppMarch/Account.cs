0d0f19fdcd0438d08a2e9322b63590a3b1c4aa5a
adeodato01 <arvin_deodato@hotmail.com>
2019-03-15 23:16:51 -0700

    Added methods and xml comments

--- BankingAppMarch/Account.cs
+++ BankingAppMarch/Account.cs
@@ -4,12 +4,56 @@ using System.Text;
 
 namespace BankingAppMarch
 {
+    /// <summary>
+    /// Account that represents
+    /// bank account where you can 
+    /// withdraw or deposit money
+    /// </summary>
     class Account
     {
+        #region Properties
+        /// <summary>
+        /// Unique number for the account
+        /// </summary>
         public int AccountNumber { get; set; }
+        /// <summary>
+        /// email address of the account holder
+        /// </summary>
         public string EmailAddress { get; set; }
+        /// <summary>
+        /// the dollar amount of the account
+        /// </summary>
         public decimal Balance { get; set; }
+        /// <summary>
+        /// description of the type of account
+        /// </summary>
         public string AccountType { get; set; }
+        /// <summary>
+        /// date the account was created
+        /// </summary>
         public DateTime CreatedDate { get; set; }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">Amount to be deposited</param>
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        /// <summary>
+        /// Withdraw money from your account
+        /// </summary>
+        /// <param name="amount">Amount to be withdrawn</param>
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        #endregion
     }
 }
