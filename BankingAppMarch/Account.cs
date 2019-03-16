3579f56e115871942e6cedd261561bc80f839728
adeodato01 <arvin_deodato@hotmail.com>
2019-03-15 22:54:42 -0700

    First Commit

---
+++ BankingAppMarch/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingAppMarch
+{
+    class Account
+    {
+        public int AccountNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal Balance { get; set; }
+        public string AccountType { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
